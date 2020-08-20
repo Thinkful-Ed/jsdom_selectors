13052ba346fa17899d8bc7c21d7adc1f4b862688
Vincent Ramdhanie <vramdhanie@gmail.com>
2020-08-19 21:30:42 -0400

    Initial files for practice

---
+++ index.js
@@ -0,0 +1,17 @@
+// The following code will be executed if this file is properly connected to the HTML
+console.log("Connected!");
+
+// Write your solution code here
+
+// 1. Select all ratings values
+const ratings = ...
+console.log(ratings);
+
+// 2. Select all stars that are not shaded
+
+
+// 3. Select the entire Gateway Arch park element by id
+
+
+// 4. Select the element with the established date value for Gateway Arch
+
